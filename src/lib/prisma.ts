96a7de6cdf200db140c0d4cfd5775c1a9445141a
Hammad Ahmad <hammadahmad4936@gmail.com>
2026-08-21 01:47:48 +0500

    Inject fallback DATABASE_URL to fix Vercel crashes if env var is missing

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "postgresql://postgres.vtjhcohylmmcffquuoyz:PaKis#09871@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true";
+}
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient();
